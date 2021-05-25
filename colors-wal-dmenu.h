776c51399a9659bd771e1a59aa402ecf27fa3a07
wolf-mathieu <80675742+wolf-mathieu@users.noreply.github.com>
2021-05-25 15:22:43 +0000

    Add files via upload

---
+++ colors-wal-dmenu.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {{
+	/*     fg         bg       */
+	[SchemeNorm] = {{ "{color15}", "{color0}" }},
+	[SchemeSel] = {{ "{color15}", "{color1}" }},
+	[SchemeOut] = {{ "{color15}", "{color14}" }},
+	[SchemeNormHighlight] = {{ "{color13}", "{color10}" }},
+	[SchemeSelHighlight] = {{ "{color13}", "{color11}" }},
+	[SchemeOutHighlight] = {{ "{color13}", "{color12}" }},
+}};
