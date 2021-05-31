80ec60e499ab1cf4fde4f72a10f683b0e0861297
wolf-mathieu <80675742+wolf-mathieu@users.noreply.github.com>
2021-05-31 16:20:58 +0000

    Add files via upload

---
+++ hd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+nvidia-settings -a CurrentMetaMode="DP-0: 1920x1080_144 @1280x720 +0+0 {ViewPortIn=1280x720, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-0: nvidia-auto-select @1920x1080 +1440+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
