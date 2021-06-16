d8d8777881f100c8b6ee89d4639739ed307bb18d
wolf-mathieu <80675742+wolf-mathieu@users.noreply.github.com>
2021-06-16 20:29:12 +0000

    Update config.def.h

--- dmenu_better/config.def.h
+++ dmenu_better/config.def.h
@@ -9,7 +9,7 @@ static const char *fonts[] = {
 	"monospace:size=10",
 	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
-static const unsigned int bgalpha = 0xe0;
+static const unsigned int bgalpha = 120;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
