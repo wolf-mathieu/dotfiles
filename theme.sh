965b4e02b6ea7c3dc4e2f9b89da68992afed1830
wolf-mathieu <80675742+wolf-mathieu@users.noreply.github.com>
2021-06-29 09:49:27 +0000

    Add files via upload

---
+++ theme.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+(cd $HOME/dotfiles/pywal && pip3 install --user . && export PATH="${PATH}:${HOME}/.local/bin/") &&
+wal --theme random &&
+(cd $HOME/dotfiles/dmenu_better && sudo make clean install) &&
+(cd $HOME/dotfiles/dwm && sudo make clean install) &&
+(cd $HOME/dotfiles/slstatus && sudo make clean install) &&
+(mkdir $HOME/.config && mkdir $HOME/.config/ranger && cp $HOME/dotfiles/ranger $HOME/.config/ranger) &&
+(mkdir $HOME/.config/picom && cp $HOME/dotfiles/picom $HOME/.config/picom)
+
+
+
+
