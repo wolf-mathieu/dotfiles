2b6eb597be953b41150680d0686636348c8c2793
wolf-mathieu <80675742+wolf-mathieu@users.noreply.github.com>
2021-06-29 13:20:56 +0200

    Add files via upload

---
+++ shel.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i "s/USER/$USER/g" $HOME/dotfiles/dwm/config.def.h
--- theme.sh
+++ theme.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
-(cd $HOME/dotfiles/pywal && pip3 install --user .) && 
-export PATH="${PATH}:${HOME}/.local/bin/" &&
+#!/bin/zsh
+(sudo apt install git python3-pip ranger libx11-dev libxft-dev libxinerama-dev libxft-dev make) &&
+(cd $HOME/dotfiles/pywal && pip3 install --user . ) &&
+(cd $HOME/dotfiles && cp $HOME/dotfiles/.zshrc $HOME/.zshrc) &&
+#(cat export PATH="${PATH}:${HOME}/.local/bin/" > $HOME/.zshrc) &&
 wal --theme random &&
+#(sed -i 's/USER/$HOME/g' $HOME/dotfiles/dwm/config.def.h) &&
+(./shel.sh) &&
+(cp $HOME/dotfiles/dwm/config.def.h $HOME/dotfiles/dwm/config.h) &&
 (cd $HOME/dotfiles/dmenu_better && sudo make clean install) &&
 (cd $HOME/dotfiles/dwm && sudo make clean install) &&
 (cd $HOME/dotfiles/slstatus && sudo make clean install) &&
-(mkdir $HOME/.config && mkdir $HOME/.config/ranger && cp $HOME/dotfiles/ranger $HOME/.config/ranger) &&
-(mkdir $HOME/.config/picom && cp $HOME/dotfiles/picom $HOME/.config/picom)
-
+(cp -r $HOME/dotfiles/ranger $HOME/.config) &&
+(cp -r $HOME/dotfiles/picom $HOME/.config) &&
+(print type your password and change shel to zsh by typing /bin/zsh && chsh && /bin/zsh) &&
 
 
 
